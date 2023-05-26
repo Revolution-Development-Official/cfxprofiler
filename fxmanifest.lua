296aa596adb314afbdacd549b40868db6c6bb2dc
NoShitSherlockium <80778117+NoShitSherlockium@users.noreply.github.com>
2023-05-25 19:29:10 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'NoShitSherlockium#9919'
+description 'Easy Parser for Profiler, helps determine what scripts are causing hitches'
+version '1.0.0'
+server_script 'server.lua'
